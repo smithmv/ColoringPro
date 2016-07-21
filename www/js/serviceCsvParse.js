9123bb5d23adca92c6a6e1bca7462625dfe5babf
Maxim Kuznetsov <maxim.k@mobile.agima.ru>
2016-07-21 15:27:31 +0400

    additional files to the previous commit

---
+++ www/js/serviceCsvParse.js
@@ -0,0 +1,31 @@
+﻿var myModule = angular.module('CSVParserService', ['SqlSaveService']);
+myModule.factory('csvparser', function ($http, sqlsave) {
+
+    var csvparser = {};
+    
+    csvparser.doParse = function () {
+        var dir = "js/lib/koren_sedina_100.js";
+            
+            var file = (ionic.Platform.isAndroid()) ? dir : cordova.file.applicationDirectory + "www/" + dir;
+
+        Papa.parse(file, {
+            encoding: "UTF-8",
+            download: true,
+            error: function(err, file) {
+                console.log(">>>> PAPA PARSE ERROR");
+                console.log(">>>>" + err); // this returns undefined
+                console.log(">>>>" + file); // this returns undefined as well
+            },
+            complete: function(results) {
+                sqlsave.createDatabase();
+                console.log(">>>> PAPA RESULTS");
+                console.log(results);
+            },
+            header: true,
+            dynamicTyping: true
+        });   
+
+        return "";
+    }
+    return csvparser;
+});
---
+++ www/js/serviceSqlSave.js
@@ -0,0 +1,47 @@
+﻿var myModule = angular.module('SqlSaveService', ['ngCordova']);
+myModule.factory('sqlsave', function ($cordovaSQLite) {
+
+    var sqlsave = {};
+    
+    sqlsave.saveData = function () {
+         
+    }
+
+    sqlsave.createDatabase = function () {
+        console.log(">>>> CREATING DATABASE");
+        var db = null;
+        if (ionic.Platform.isAndroid()) {
+            // Works on android but not in iOS
+            db = $cordovaSQLite.openDB({ name: "rules.db", iosDatabaseLocation: 'default' });
+        } else {
+            // Works on iOS 
+            db = window.sqlitePlugin.openDatabase({ name: "rules.db", location: 2, createFromLocation: 1 });
+        }
+        var qKorenSedina100 = "CREATE table KORENSEDINA100 (id INTEGER PRIMARY KEY ASC AUTOINCREMENT, " +
+            "stepNumber INTEGER DEFAULT 0, " +
+            "isVK INTEGER DEFAULT 0, " +
+            "isKNR INTEGER DEFAULT 0, " +
+            "KNR TEXT DEFAULT NULL, " +
+            "isOXI INTEGER DEFAULT 0, " +
+            "OXIPercent TEXT DEFAULT NULL," +
+            "timeMinutes INTEGER DEFAULT NULL, " +
+            "isToning INTEGER DEFAULT 0" +
+            "isPP INTEGER DEFAULT 0";
+
+        var qKorenSedina100LinkTable = "CREATE table KORENSEDINA100LINK (id INTEGER PRIMARY KEY ASC AUTOINCREMENT, " +
+            "startLevel INTEGER NOT NULL, " +
+            "finishLevel INTEGER NOT NULL, " +
+            "recId INTEGER DEFAULT NULL, " +
+            "FOREIGN KEY(recId) REFERENCES KORENSEDINA100(id)";
+
+        $cordovaSQLite.execute(db, qKorenSedina100).then(function (res) {
+        });
+
+        $cordovaSQLite.execute(db, qKorenSedina100LinkTable).then(function (res) {
+        });
+
+        console.log(">>>> CREATING DATABASE FINISHED");
+    }
+
+    return sqlsave;
+});
